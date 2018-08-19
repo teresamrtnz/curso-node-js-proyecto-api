f96b9c1c6f89960d4076f557d9fe0600b3092de9
juanda <juandacorreo@gmail.com>
2018-08-19 19:16:40 +0200

    remove routes for exercise

--- app/server.js
+++ app/server.js
@@ -11,14 +11,6 @@
     res.json({ mensaje: '¡A beber cerveza!' })  
   })
   
-  app.post('/', function(req, res) {
-    res.json({ mensaje: 'Método post' })   
-  })
-  
-  app.del('/', function(req, res) {
-    res.json({ mensaje: 'Método delete' })  
-  })
-
   // iniciamos nuestro servidor
   app.listen(port)
   console.log('API escuchando en el puerto ' + port)
