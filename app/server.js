327f05e18887b0a1076f71bfa472aeaeade061fc
juanda <juandacorreo@gmail.com>
2018-08-19 19:23:23 +0200

    lint fixes and arrow functions

--- app/server.js
+++ app/server.js
@@ -1,17 +1,16 @@
-  var express = require('express') //llamamos a Express
-  var app = express()
+var express = require('express') // llamamos a Express
+var app = express()
 
-  var port = process.env.PORT || 8080  // establecemos nuestro puerto
+var port = process.env.PORT || 8080 // establecemos nuestro puerto
 
-  app.get('/', function(req, res) {
-    res.json({ mensaje: '¡Hola Mundo!' })
-  })
-  
-  app.get('/cervezas', function(req, res) {
-    res.json({ mensaje: '¡A beber cerveza!' })  
-  })
-  
-  // iniciamos nuestro servidor
-  app.listen(port)
-  console.log('API escuchando en el puerto ' + port)
+app.get('/', (req, res) => {
+  res.json({ mensaje: '¡Hola Mundo!' })
+})
 
+app.get('/cervezas', (req, res) => {
+  res.json({ mensaje: '¡A beber cerveza!' })
+})
+
+// iniciamos nuestro servidor
+app.listen(port)
+console.log('API escuchando en el puerto ' + port)
